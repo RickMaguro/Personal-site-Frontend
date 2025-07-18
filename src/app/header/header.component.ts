e93b165c2917c62d15b4387794a583da0826e621
Rick Chang <rick800721@gmail.com>
2025-07-18 16:57:38 +0100

    Enhance mobile layout, implement responsive (burger) header menu, and update resume content

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import {faGithub,faLinkedin,faBitbucket} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin, faBitbucket } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'] 
 })
 export class HeaderComponent {
   faGithub = faGithub;
   faLinkedin = faLinkedin;
-  faBitbucket = faBitbucket
+  faBitbucket = faBitbucket;
+
+  ngAfterViewInit(): void {
+    // Make sure the burger menu is working on the browser
+    if (typeof document !== 'undefined') {
+      const burger = document.querySelector('.navbar-burger');
+      const menu = document.querySelector('.navbar-menu');
+      const navItems = document.querySelectorAll('.navbar-item');
+  
+      // Switch the burger menu
+      burger?.addEventListener('click', () => {
+        burger.classList.toggle('is-active');
+        menu?.classList.toggle('is-active');
+      });
+  
+      // When each page is clicked, close the menu
+      navItems.forEach((item) => {
+        item.addEventListener('click', () => {
+          if (burger?.classList.contains('is-active')) {
+            burger.classList.remove('is-active');
+            menu?.classList.remove('is-active');
+          }
+        });
+      });
+    }
+  }
 }
